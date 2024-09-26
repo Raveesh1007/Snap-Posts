ddedf9402d6201db2ab33b4607972b1153061fb8
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-09-26 22:07:46 +0530

    Added different post sizes for different posts

---
+++ src/app/components/postSizes.ts
@@ -0,0 +1,10 @@
+import { AiFillInstagram, AiFillLinkedin, AiFillFacebook, AiFillTwitterCircle } from "react-icons/ai";
+
+export const SizeList =[
+    {size: 'igstory', width: 1080, height: 1920, Icon: AiFillInstagram},
+    {size: 'igpost', width: 1080, height: 1080, Icon: AiFillInstagram},
+    {size: 'ig post potrait', width: 1080, height: 1350, Icon: AiFillInstagram},
+    {size: 'fb post', width: 1200, height: 630, Icon: AiFillFacebook},
+    {size: 'Linkedin post' , width: 1200, height: 627, Icon: AiFillLinkedin},
+    {size: 'Twitter post', width: 1200, height: 675, Icon: AiFillTwitterCircle},
+];
