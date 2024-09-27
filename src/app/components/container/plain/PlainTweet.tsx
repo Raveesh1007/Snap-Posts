58196bb92874ecbea7afa135e27d1346a041aea6
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-09-27 18:04:45 +0530

    Done a bunch of changes

---
+++ src/app/components/container/plain/PlainTweet.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {
+    PlainProfile,
+    PlainContent,
+    PlainTime,
+    PlainResponse,
+} from "@/components";
+
+import { AppContext } from "@/pages";
+import { useContext } from "react";
