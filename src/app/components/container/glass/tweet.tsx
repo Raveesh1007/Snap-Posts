ed6b9ded8959211e7ef51c597d7ba471bf6a299a
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-09-29 19:29:31 +0530

    what to write

---
+++ src/app/components/container/glass/tweet.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {Profile, content, Time, Response} from "@/components";
+import { AppContext } from "@/pages";
+import { useContext } from "react";
+
+const Tweet = () = {
+    const {response, cardColor} = useContext(AppContext);
+}
