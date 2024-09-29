81635f046d30b00b558ce9454c5d39865f1fb0ad
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-09-29 19:31:55 +0530

    tweet.tsx completed

--- src/app/components/container/glass/tweet.tsx
+++ src/app/components/container/glass/tweet.tsx
@@ -3,6 +3,28 @@ import {Profile, content, Time, Response} from "@/components";
 import { AppContext } from "@/pages";
 import { useContext } from "react";
 
-const Tweet = () = {
-    const {response, cardColor} = useContext(AppContext);
-}
+const Tweet = () => {
+    const { response, cardColor } = useContext(AppContext);
+  
+    return (
+      <div
+        className={`
+      ${
+        cardColor === "Dark"
+          ? "glass_morphic_card__dark"
+          : "glass_morphic_card__light"
+      }
+      shadow-xl h-fit
+      p-10`}
+      >
+        <Profile />
+        <Content />
+        <div className="w-full flex mt-5 justify-between">
+          {response && <Response />}
+          <Time />
+        </div>
+      </div>
+    );
+  };
+  
+  export default Tweet;
