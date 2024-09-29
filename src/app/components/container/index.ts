3441b7582de4c9d0a90ef334538a271a82745cda
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-09-29 19:50:25 +0530

    index.ts of plain components

---
+++ src/app/components/container/index.ts
@@ -0,0 +1,10 @@
+import Bottombar from "./Bottombar";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Tweet from "./glass/tweet";
+import Body from "./glass/glass_body";
+import PlainTweet from "./plain/PlainTweet";
+import PlainBody from "./plain/plain_body";
+
+export { Bottombar, Navbar, Sidebar, Tweet, Body, PlainTweet, PlainBody };
+
