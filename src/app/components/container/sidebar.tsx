afe655de13221e9c02ae2445f24216147faf1c9d
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-09-30 18:08:03 +0530

    Sidebar added

---
+++ src/app/components/container/sidebar.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useContext } from 'react'
+import { AppContext } from '@/pages'
+import { BackgroundColor, BackgroundType, 
+  CardColor,PlainColors, Responses,
+   Size,Download,PostSize,TextColor } from '@/components'
+import exp from 'constants'
+
+   const SideBar = () => {
+    const {bgType, favicon} =  useContext(AppContext);
+
+    return (
+        <div className='absolute top-0 left-0 h-screen w-24 m-0 flex flex-col shadow-lg z-20'>
+          {
+            bgType === 'Plain' ? <PlainColors /> : <BackgroundColor/>
+          }
+
+          <BackgroundType/ >
+          {
+            bgType === 'Glass' ? <CardColor />: <TextColor/>
+          }
+          <Responses/>
+          <Size />
+          <PostSize />
+          <Download />
+        </div>
+    )
+  }
+
+export default SideBar;
