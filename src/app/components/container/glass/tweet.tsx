96dd20413eaa02979838cfb9da780a5e54033bbc
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-10-01 20:32:02 +0530

    tweet.tsx changes

--- src/app/components/container/glass/tweet.tsx
+++ src/app/components/container/glass/tweet.tsx
@@ -4,7 +4,7 @@ import { AppContext } from "@/pages";
 import { useContext } from "react";
 
 const Tweet = () => {
-    const { response, cardColor } = useContext(AppContext);
+    const { Response, cardColor } = useContext(AppContext);
   
     return (
       <div
