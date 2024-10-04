c3162594bafc704514d71566832f7f5c4d9a641a
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-10-04 19:34:47 +0530

    added time

---
+++ src/app/components/tweet/glass/time.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useContext } from "react";
+import { AppContext } from "../../../pages";
+
+function convertDate(dateStr: string): string {
+    const date = new Date(dateStr);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const day = date.getDate();
+    const month = date.toLocaleString("default", { month: "short" });
+    const year = date.getFullYear();
+    const AmPm = date.getHours() >= 12 ? "PM" : "AM";
+    const hour12 = date.getHours() % 12 || 12;
+    const timeString = `${hour12}:${minutes.toString().padStart(2, '0')} ${AmPm}`;
+    const dateString = `${day} ${month} ${year}`;
+
+    return `${timeString} · ${dateString}`;
+}
+
+const Time = () => {
+    const { tweet, cardColor } = useContext(AppContext);
+    
+    const createdTime = convertDate(tweet?.created_at || "Friday Oct 4 19:33:00 2024")
+
+    return (
+        <p className={`${ cardColor === "Dark" ? ' text-gray-200 ' : 'text-gray-700 '} w-full mx-2 text-lg opacity-60 `}>
+          {createdTime}
+        </p>
+    );
+}
+
+
+export default Time;
