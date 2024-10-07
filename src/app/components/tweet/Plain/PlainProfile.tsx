df80e8b61a884d4a8bca19814a7f30dd6c25eb50
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-10-07 16:41:34 +0530

    completed PLain profile

--- src/app/components/tweet/Plain/PlainProfile.tsx
+++ src/app/components/tweet/Plain/PlainProfile.tsx
@@ -13,7 +13,23 @@ const PlainProfile =() =>{
             <div className="flex justify-start">
                 <Image 
                     className="rounded-full w-10 h-10 mx-2"
+                    src = {tweet?.user?.profile_image_url_https || "/images/profile.webp"}
+                    alt = "profile"
+                    width = {30}
+                    height = {30}
+                />
+                <div className= "flex flex-col">
+                <p className={`text-md mb-[-3px] ${textColor}`}>
+                {tweet?.user.name || "Raveesh Kumar"}
+                </p>
+                <p className={`text-md mb-[-3px] ${textColor}`}>
+                {tweet?.user.name || "Raveesh Kumar"}
+                </p>
+                </div>
             </div>
+            <BsTwitter className="text-[#1BA0F0] w-6 h-6" />
         </div>
-    )
-}
+    );
+};
+
+export default PlainProfile;
